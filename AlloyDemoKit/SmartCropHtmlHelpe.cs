8fbc3bd069b0d391c78ea307c71e2ca7b1042350
marcin.sikora <marcinsikora@outlook.com>
2019-06-14 11:49:33 +0200

    Resizing adjustments

--- AlloyDemoKit/SmartCropHtmlHelpe.cs
+++ AlloyDemoKit/SmartCropHtmlHelpe.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 using AlloyDemoKit.Models.Media;
@@ -34,7 +35,7 @@ namespace AlloyDemoKit
 
             if (smartCrop && isCrop)
             {
-                parameters.Add("crop=" + CalculateCropBounds(imageFile, width, height));
+                parameters.Add("crop=" + CalculateCropBounds(imageFile, width.Value, height.Value));
             }
 
             if (width != null)
@@ -62,21 +63,83 @@ namespace AlloyDemoKit
 
         }
 
-        private static string CalculateCropBounds(ImageFile imageFile, int? width, int? height)
+        private static string CalculateCropBounds(ImageFile imageFile, int width, int height)
         {
-            int x = imageFile.AreaOfInterestX;
-            int y = imageFile.AreaOfInterestY;
-            int w = imageFile.AreaOfInterestWidth;
-            int h = imageFile.AreaOfInterestHeight;
+	        using (var stream = ReadBlob(imageFile))
+	        {
+		        var originalImage = Image.FromStream(stream);
+		     
 
-            return $"{x},{y},{w},{h}";
+		        var areaX = imageFile.AreaOfInterestX;
+		        var areaY = imageFile.AreaOfInterestY;
+				var areaW = imageFile.AreaOfInterestWidth;
+				var areaH = imageFile.AreaOfInterestHeight;
+
+
+				double cropRatio = width / (double)height;
+				double originalRatio = originalImage.Width / (double)originalImage.Height;
+
+				var cropQuery = string.Empty;
+				if (cropRatio < originalRatio)
+				{
+					var boundingRectHeight = originalImage.Height;
+					var boundingRectWidth = boundingRectHeight * cropRatio;
+
+					var xFocalPoint = areaW / 2 + areaX;
+					var cropX = xFocalPoint - boundingRectWidth / 2;
+					if (cropX < 0)
+						cropX = 0;
+
+					if (cropX + boundingRectWidth > originalImage.Width)
+						cropX = originalImage.Width - boundingRectWidth;
+
+					var cropY = 0.0;
+
+					cropQuery = $"{cropX},{cropY},{cropX + boundingRectWidth},{cropY + boundingRectHeight}";
+				}
+				else
+				{
+					var boundingRectWidth = originalImage.Width;
+					var boundingRectHeight = boundingRectWidth / cropRatio;
+
+
+
+					var yFocalPoint = areaH / 2 + areaY;
+					var cropY = yFocalPoint - boundingRectHeight / 2;
+					if (cropY < 0)
+						cropY = 0;
+
+					if (cropY + boundingRectHeight > originalImage.Height)
+						cropY = originalImage.Height - boundingRectHeight;
+
+					var cropX = 0.0;
+
+					cropQuery = $"{cropX},{cropY},{cropX + boundingRectWidth},{cropY + boundingRectHeight}";
+				}
+
+				return cropQuery;
+	        }
         }
 
         private static string ResolveImageUrl(ContentReference image)
         {
             return UrlResolver.Current.GetUrl(image);
         }
-    }
+
+        private static MemoryStream ReadBlob(ImageFile content)
+        {
+	        using (var stream = content.BinaryData.OpenRead())
+	        {
+		        var buffer = new byte[stream.Length];
+		        stream.Read(buffer, 0, buffer.Length);
+
+		        var memoryStream = new MemoryStream(buffer, writable: false);
+		        return memoryStream;
+	        }
+        }
+	}
+
+
 
     public class SmartCropCalculator
     {
