b93d3b2775aea95c0e329e2a89f7e6610c58b985
Mikołaj Dłuś <mikolaj.dlus@fortedigital.no>
2019-07-16 10:19:06 +0200

    Work in progress

---
+++ SmartCrop/Business/Plugins/SmartCropAdminPluginController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.PlugIn;
+using EPiServer.Security;
+using Forte.SmartCrop.Models.ViewModels;
+
+namespace Forte.SmartCrop.Business.Plugins
+{
+    [GuiPlugIn(DisplayName = "Manage SmartCrop", Area = PlugInArea.AdminMenu,
+        Url = "/custom-plugins/smartcrop-plugin")]
+    public class SmartCropAdminPluginController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            var images = GetAllImages();
+
+            var model = new SmartCropAdminPluginViewModel {ImagesEnumerable = images};
+
+            return View("~/Business/Plugins/Views/Index.cshtml", model);
+        }
+
+        private static IEnumerable<ImageData> GetAllImages()
+        {
+            return Enumerable.Empty<ImageData>();
+        }
+    }
+}
---
+++ SmartCrop/Models/ViewModels/SmartCropAdminPluginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Core;
+
+namespace Forte.SmartCrop.Models.ViewModels
+{
+    public class SmartCropAdminPluginViewModel
+    {
+        public IEnumerable<ImageData> ImagesEnumerable { get; set; }
+        
+    }
+}
