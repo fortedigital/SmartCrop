f5faf2f83fd0d3df85ef464e7bf9375c8f0933f7
Mikołaj Dłuś <mikolaj.dlus@fortedigital.no>
2019-07-22 08:54:13 +0200

    updated module name

--- SmartFocalPoint/copy_module.bat
+++ SmartFocalPoint/copy_module.bat
@@ -1,2 +1,2 @@
-rd /S /Q "..\AlloyDemoKit\modules\_protected\Forte.SmartCrop"
-xcopy /E /K /H /Y /I "Plugins" "..\AlloyDemoKit\modules\_protected\Forte.SmartCrop"
+rd /S /Q "..\AlloyDemoKit\modules\_protected\Forte.SmartFocalPoint"
+xcopy /E /K /H /Y /I "Plugins" "..\AlloyDemoKit\modules\_protected\Forte.SmartFocalPoint"
